bdeefa7c219dc466aab9417356b63bfbc934583f
Niall Murphy <n.murphy46@nuigalway.ie>
2020-11-15 18:14:31 +0000

    v5 DateTime Fix

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,39 @@
+
+import org.joda.time.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User1
+ */
+public class TestClass {
+    
+    public static void main(String[] args){
+        TestClass test= new TestClass();
+        test.registration();
+    }
+    
+    public void registration(){
+        //init localdates for start and end
+        DateTime start = DateTime.parse("2020-09-28");
+        DateTime end = DateTime.parse("2021-06-04");
+        
+        //CourseProgramme course1 = new CourseProgramme(start,end,"ECE");
+        
+        
+        
+        //initialise some students
+        /**Student stud1 = new Student("John Jacobs",20,"ECE","CT-417");
+        Student stud2 = new Student("Lynn Hunt",19,"IT","CT-417");
+        Student stud3 = new Student("Karen Leahy",20,"CS","CT-417");
+        Student stud4 = new Student("Daniel Carr",20,"EEE","CT-417");
+        **/
+        
+    }
+    
+}
