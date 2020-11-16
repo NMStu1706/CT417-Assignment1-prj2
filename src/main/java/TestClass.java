e7b1e108b98ed4ea089ee3c10b06bbb515a63a4f
Niall Murphy <n.murphy46@nuigalway.ie>
2020-11-16 16:43:08 +0000

    v7 clean up for test class

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -22,17 +22,36 @@ public class TestClass {
         //init localdates for start and end
         DateTime start = DateTime.parse("2020-09-28");
         DateTime end = DateTime.parse("2021-06-04");
+        //init the course,module and student objects
+        CourseProgramme course1 = new CourseProgramme(start,end,"ECE");
+        CourseProgramme course2 = new CourseProgramme(start,end,"CS");
         
-        //CourseProgramme course1 = new CourseProgramme(start,end,"ECE");
-        
-        
+        Module mod1= new Module("Software Engineering III","CT417","CS,ECE");
+        Module mod2= new Module("Digital Signal Processing","EE445","ECE");
+        //add relevant modules to courses
+        course1.addModule(mod1);
+        course1.addModule(mod2);
+        course2.addModule(mod1); //cs does not have digital signal processing module in this case
         
         //initialise some students
-        /**Student stud1 = new Student("John Jacobs",20,"ECE","CT-417");
-        Student stud2 = new Student("Lynn Hunt",19,"IT","CT-417");
-        Student stud3 = new Student("Karen Leahy",20,"CS","CT-417");
-        Student stud4 = new Student("Daniel Carr",20,"EEE","CT-417");
-        **/
+        Student stud1 = new Student("John Jacobs",20,"ECE","CT417,EE445");
+        Student stud2 = new Student("Lynn Hunt",19,"ECE","CT417,EE445");
+        Student stud3 = new Student("Karen Leahy",20,"CS","CT417");
+        Student stud4 = new Student("Daniel Carr",20,"CS","CT417");
+        //add relevant students to courses and mods
+        course1.addStudent(stud1);
+        course1.addStudent(stud2);
+        course2.addStudent(stud3);
+        course2.addStudent(stud4);
+        
+        mod1.addStudent(stud1);
+        mod1.addStudent(stud2);
+        mod1.addStudent(stud3);
+        mod1.addStudent(stud4);
+        //cs does not to digital signal processing in this case
+        mod2.addStudent(stud1);
+        mod2.addStudent(stud2);
+        //
         
     }
     
